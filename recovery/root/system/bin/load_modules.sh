b1eb9a417521418fefabc42ec56c8a6585aefabe
uotlaf <uotlaf@protonmail.com>
2022-03-03 13:39:03 -0300

    Update load_modules.sh location

--- recovery/root/system/bin/load_modules.sh
+++ recovery/root/system/bin/load_modules.sh
@@ -2,9 +2,9 @@
 
 
 # Install touchscreen modules
-insmod /vendor/lib/modules/nova_0flash_mmi.ko
-insmod /vendor/lib/modules/himax_v2_mmi_hx83102d.ko
-insmod /vendor/lib/modules/himax_v2_mmi.ko
+insmod /vendor/lib/modules/1.1/nova_0flash_mmi.ko
+insmod /vendor/lib/modules/1.1/himax_v2_mmi_hx83102d.ko
+insmod /vendor/lib/modules/1.1/himax_v2_mmi.ko
 
 # Update novatek touchscreen firmware early
 cat /proc/nvt_update
