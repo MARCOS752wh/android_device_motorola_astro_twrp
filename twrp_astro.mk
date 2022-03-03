4abca50890ecb735dcb45c4f3e510c69e0ef0858
uotlaf <uotlaf@protonmail.com>
2022-03-03 13:36:31 -0300

    Remove unnecessary lines in twrp_astro.mk

--- twrp_astro.mk
+++ twrp_astro.mk
@@ -19,11 +19,6 @@ PRODUCT_RELEASE_NAME := astro
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
-#$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-#$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
-#$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-#$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
-
 
 # Inherit some common TWRP stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
